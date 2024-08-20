25709877a064126bd877839b34e5f389a6082714
eric4477 <ericgeorge4477@gmail.com>
2024-08-20 16:07:37 +0300

    0-console.py

---
+++ 0x05-Node_JS_basic/0-console.js
@@ -0,0 +1,14 @@
+/**
+ * @file 0-console.js
+ * @description This module exports a function that prints a given message to the console (STDOUT).
+ */
+
+/**
+ * Prints the provided message to the console.
+ * @param {string} message - The message to be printed.
+ */
+function displayMessage(message) {
+  console.log(message);
+}
+
+module.exports = displayMessage;
